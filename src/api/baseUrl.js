7b7de9ab66ec72bad1ca79c763fb28b46997e77d
Hitendra Rajput <hitendra.http@gmail.com>
2020-04-06 00:33:35 +0530

    add deploy script for UI

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://safe-tor-03129.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
